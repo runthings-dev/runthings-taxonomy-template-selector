ca00816660f658be6b8d88b6d44778796b321123
Matthew Harris <rtpharry@hotmail.com>
2025-12-15 13:04:57 +0000

    refactor: split into separate classes in lib folder

---
+++ lib/Admin.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * Admin UI handler
+ *
+ * @package Runthings_Taxonomy_Template
+ */
+
+namespace Runthings\TaxonomyTemplate;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Handles admin UI for taxonomy template selection
+ */
+class Admin {
+
+	/**
+	 * Initialize admin hooks
+	 */
+	public function __construct() {
+		add_action( 'init', array( $this, 'register_taxonomy_hooks' ), 99 );
+	}
+
+	/**
+	 * Register hooks for all public taxonomies
+	 *
+	 * @return void
+	 */
+	public function register_taxonomy_hooks() {
+		$taxonomies = get_taxonomies( array( 'public' => true ), 'names' );
+
+		foreach ( $taxonomies as $taxonomy_name ) {
+			add_action( $taxonomy_name . '_edit_form_fields', array( $this, 'render_meta_box' ) );
+			add_action( $taxonomy_name . '_add_form_fields', array( $this, 'render_meta_box' ) );
+			add_action( 'created_' . $taxonomy_name, array( $this, 'save_template' ) );
+			add_action( 'edited_' . $taxonomy_name, array( $this, 'save_template' ) );
+		}
+	}
+
+	/**
+	 * Get category templates from theme
+	 *
+	 * @return array
+	 */
+	public function get_category_templates() {
+		$post_templates = array();
+
+		$theme_dir      = get_template_directory();
+		$stylesheet_dir = get_stylesheet_directory();
+
+		$dirs_to_scan = array( $theme_dir );
+		if ( $stylesheet_dir !== $theme_dir ) {
+			$dirs_to_scan[] = $stylesheet_dir;
+		}
+
+		foreach ( $dirs_to_scan as $dir ) {
+			$files = glob( $dir . '/*.php' );
+			if ( ! is_array( $files ) ) {
+				continue;
+			}
+
+			foreach ( $files as $template ) {
+				$basename = basename( $template );
+				if ( 'functions.php' === $basename ) {
+					continue;
+				}
+
+				$template_data = file_get_contents( $template );
+				if ( false === $template_data ) {
+					continue;
+				}
+
+				$name = '';
+				if ( preg_match( '|Category Template:(.*)$|mi', $template_data, $name ) ) {
+					$name = trim( preg_replace( '/\s*(?:\*\/|\?>).*/', '', $name[1] ) );
+				}
+
+				if ( ! empty( $name ) ) {
+					$post_templates[ trim( $name ) ] = $basename;
+				}
+			}
+		}
+
+		return $post_templates;
+	}
+
+	/**
+	 * Render meta box on taxonomy edit screen
+	 *
+	 * @param mixed $tag Term object or empty.
+	 * @return void
+	 */
+	public function render_meta_box( $tag ) {
+		$term_id = '';
+		if ( ! empty( $tag ) && is_object( $tag ) ) {
+			$term_id = $tag->term_id;
+		}
+		$template_mappings = get_option( 'runthings_taxonomy_template_mappings' );
+		$selected_template = isset( $template_mappings[ $term_id ] ) ? $template_mappings[ $term_id ] : false;
+		?>
+		<?php wp_nonce_field( 'runthings_taxonomy_template_nonce_action', 'runthings_taxonomy_template_nonce_field' ); ?>
+		<tr class="form-field">
+			<th scope="row" valign="top"><label for="taxonomy_template"><?php esc_html_e( 'Taxonomy Template', 'runthings-taxonomy-template' ); ?></label></th>
+			<td>
+				<select name="taxonomy_template" id="taxonomy_template">
+					<option value='default'><?php esc_html_e( 'Default Template', 'runthings-taxonomy-template' ); ?></option>
+					<?php $this->render_template_dropdown( $selected_template ); ?>
+				</select>
+			</td>
+		</tr>
+		<?php
+	}
+
+	/**
+	 * Render template dropdown options
+	 *
+	 * @param string $default Currently selected template.
+	 * @return void
+	 */
+	public function render_template_dropdown( $default = '' ) {
+		$templates = $this->get_category_templates();
+		ksort( $templates );
+		foreach ( array_keys( $templates ) as $template ) {
+			$selected = ( $default === $templates[ $template ] ) ? ' selected="selected"' : '';
+			echo "\n\t<option value='" . esc_attr( $templates[ $template ] ) . "'" . esc_attr( $selected ) . '>' . esc_html( $template ) . '</option>';
+		}
+	}
+
+	/**
+	 * Save template selection
+	 *
+	 * @param int $term_id Term ID.
+	 * @return void
+	 */
+	public function save_template( $term_id ) {
+		if ( ! isset( $_POST['runthings_taxonomy_template_nonce_field'] ) ) {
+			return;
+		}
+
+		$nonce = sanitize_text_field( wp_unslash( $_POST['runthings_taxonomy_template_nonce_field'] ) );
+		if ( ! wp_verify_nonce( $nonce, 'runthings_taxonomy_template_nonce_action' ) ) {
+			return;
+		}
+
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return;
+		}
+
+		if ( isset( $_POST['taxonomy_template'] ) ) {
+			$template_mappings = get_option( 'runthings_taxonomy_template_mappings' );
+			if ( ! is_array( $template_mappings ) ) {
+				$template_mappings = array();
+			}
+			$template_mappings[ $term_id ] = sanitize_text_field( wp_unslash( $_POST['taxonomy_template'] ) );
+			update_option( 'runthings_taxonomy_template_mappings', $template_mappings );
+		}
+	}
+}
+
---
+++ lib/Template_Loader.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Template loader
+ *
+ * @package Runthings_Taxonomy_Template
+ */
+
+namespace Runthings\TaxonomyTemplate;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Handles template filtering for taxonomy archives
+ */
+class Template_Loader {
+
+	/**
+	 * Initialize template hooks
+	 */
+	public function __construct() {
+		add_filter( 'taxonomy_template', array( $this, 'filter_taxonomy_template' ) );
+		add_filter( 'category_template', array( $this, 'filter_taxonomy_template' ) );
+	}
+
+	/**
+	 * Filter taxonomy template
+	 *
+	 * @param string $taxonomy_template Current template path.
+	 * @return string
+	 */
+	public function filter_taxonomy_template( $taxonomy_template ) {
+		$queried_term = get_queried_object();
+		if ( ! $queried_term || ! isset( $queried_term->term_id ) ) {
+			return $taxonomy_template;
+		}
+
+		$term_id = $queried_term->term_id;
+		$template_mappings = get_option( 'runthings_taxonomy_template_mappings' );
+
+		if ( isset( $template_mappings[ $term_id ] ) && 'default' !== $template_mappings[ $term_id ] ) {
+			$located_template = locate_template( $template_mappings[ $term_id ] );
+			if ( ! empty( $located_template ) ) {
+				return apply_filters( 'runthings_taxonomy_template_found', $located_template );
+			}
+		}
+
+		return $taxonomy_template;
+	}
+}
+
